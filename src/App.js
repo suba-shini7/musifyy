1032b33e44636ae80157f4e909dfa337236730d8
subashini <subashini93825@gmail.com>
2024-06-26 21:33:05 +0530

    fetching API

--- src/App.js
+++ src/App.js
@@ -7,14 +7,35 @@ import { RecentlyPlayed } from "./Components/Home/RecentlyPlayed";
 import { MyWishtlist } from "./Components/MyWishtlist";
 import { Explore } from "./Components/Explore";
 import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
 
 function App() {
+  const getMusic = async () => {
+    const url = 'https://spotify23.p.rapidapi.com/search/?q=5lfuu0un8XjAtUdxwtqjm4&type=multi&offset=0&limit=10&numberOfTopResults=5';
+    const options = {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-key': 'c214c11fd1msh0f20252d7d3bc03p14ecfdjsnba10e8984f43',
+        'x-rapidapi-host': 'spotify23.p.rapidapi.com'
+      }
+    };
+    
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    getMusic();
+  }, []);
   return (
     <div>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />}> 
-        </Route>
+        <Route path="/" element={<Home />}></Route>
         <Route path="recentlyplayed" element={<RecentlyPlayed />} />
         <Route path="mywishtlist" element={<MyWishtlist />} />
         <Route path="explore" element={<Explore />} />
